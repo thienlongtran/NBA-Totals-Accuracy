1860cdad6a1bf61b8b1aa917d55a49c97435f423
thienlongtran <ttran384@gatech.edu>
2022-12-28 03:23:47 -0600

    Created combined dataset of OU Lines and Game Scores

--- main.py
+++ main.py
@@ -13,13 +13,25 @@ def get_lines_on_day(date):
         "apiKey": THE_ODDS_API_KEY
     }
     response = requests.get(THE_ODDS_API_URL, params=params)
-    with open("./ou_lines_list/%s/%s.json" % (date.split("-")[0] ,date), "w") as f: json.dump(response.json(), f)
+    with open("./ou_lines_list/%s/%s.json" % (date.split("-")[0], date), "w") as f: json.dump(response.json(), f)
+
+def concat_ou_line_to_df(df):
+    games_with_lines = pd.DataFrame()
+    for i, game in tqdm(df.iterrows()):
+        if "%s.json" % game.Date not in os.listdir("./ou_lines_list/%s" % game.Date.split("-")[0]): get_lines_on_day(game.Date) #check if odds data exists
+        with open("./ou_lines_list/%s/%s.json" % (game.Date.split("-")[0], game.Date), "r") as read_content:
+            for game_odds in json.load(read_content)["data"]:
+                if game.Home == game_odds["home_team"] and game.Away == game_odds["away_team"]:
+                    for bookmaker in game_odds["bookmakers"]:
+                       game[bookmaker["title"]] = bookmaker["markets"][0]["outcomes"][0]["point"]
+        games_with_lines = pd.concat([games_with_lines, game], axis=1)
+    return games_with_lines.T
 
 if __name__ == "__main__":
+    df = pd.DataFrame()
     for file in os.listdir("./games_list/cleaned_data"):
         filename = os.fsdecode(file)
-        print(filename)
         if filename.endswith(".xlsx"):
-            df = pd.read_excel("./games_list/cleaned_data/" + filename)
-            for row in tqdm(df.itertuples()):
-                if "%s.json" % row.Date not in os.listdir("./ou_lines_list/%s" % row.Date.split("-")[0]): get_lines_on_day(row.Date)
+            df = pd.concat([df, pd.read_excel("./games_list/cleaned_data/" + filename)], axis=0)
+    games_with_lines = concat_ou_line_to_df(df)
+    games_with_lines.to_excel("Full_Dataset.xlsx")
